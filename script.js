2fd231ab16c793d659c53b6d3fb65c851647447e
Tuhina-Paria <pariatuhina04@gmail.com>
2026-02-25 09:10:03 +0530

    Add delete functionality, roll number identifier, and fix empty summary

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ let students = [];
 // Called when button is clicked
 function addStudent() {
   const nameInput = document.getElementById("name");
+  const rollInput = document.getElementById("roll");
+
   const marksInput = document.getElementById("marks");
 
   const name = nameInput.value;
+  const roll = Number(rollInput.value);
   const marks = Number(marksInput.value);
 
-  if (name === "" || marks === "") {
-    alert("Please enter name and marks");
+  if (name === "" || isNaN(marks) || isNaN(roll)) {
+    alert("Please enter name and marks and roll");
     return;
   }
 
-  students.push({ name, marks });
+  students.push({ name, marks,roll});
 
   nameInput.value = "";
+  rollInput.value="";
   marksInput.value = "";
 
   renderTable();
@@ -27,7 +31,7 @@ function renderTable() {
   const tableBody = document.getElementById("tableBody");
   tableBody.innerHTML = "";
 
-  students.forEach(student => {
+  students.forEach((student ,index)=> {
     const row = document.createElement("tr");
 
     const status = student.marks >= 35 ? "Pass" : "Fail";
@@ -35,15 +39,36 @@ function renderTable() {
 
         row.innerHTML = `
             <td>${student.name}</td>
+            <td>${student.roll}</td>
             <td>${student.marks}</td>
             <td class="${statusClass}">${status}</td>
+             <td>
+        <button onclick="deleteStudent(${index})">❌</button>
+      </td>
     `;
 
     tableBody.appendChild(row);
   });
 }
+function deleteStudent(index){
+  students.splice(index,1);
+  renderTable();
+  updateSummary();
+}
 
 function updateSummary() {
+
+  const averageEl = document.getElementById("average");
+  const topperEl = document.getElementById("topper");
+
+  if(students.length === 0){
+      averageEl.innerText = "";
+    topperEl.innerText = "";
+    return;
+  }
+
+
+
   const total = students.reduce((sum, s) => sum + s.marks, 0);
   const average = (total / students.length).toFixed(2);
 
